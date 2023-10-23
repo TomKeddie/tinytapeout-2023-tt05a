f0e06134530722ffd7ef06b558a752cb87a41943
Tom Keddie <github@bronwenandtom.com>
2023-10-23 06:39:06 -0700

    VGA Updates
    - added PLL for icebreaker
    - added more complete timing for sync pulses
    - fixed and verified timing on scope with icebreaker
    - fixed icebreaker pinout

--- src/top_icebreaker.v
+++ src/top_icebreaker.v
@@ -17,34 +17,64 @@ module top
    output P1B8
    );
 
-   reg [0:9] rst_counter = 10'b11_1111_1111;
+  reg [0:9] rst_counter = 10'b11_1111_1111;
 
-   reg	  rst;
+  reg       rst;
 
-   // POR delay
-   always @ (posedge CLK) begin
-      if (rst_counter != 0) begin
-	 rst <= 1'b1;
-	 rst_counter <= rst_counter - 1;
-      end else begin
-	 rst <= 1'b0;
-      end
-   end
+  wire      locked;
+  wire      clk_25_125;
 
-   vga vga(.clk(CLK), 
-	   .rst(rst),
-	   .r0(P1A1),
-	   .r1(P1A2),
-	   .r2(P1A3),
-	   .r3(P1A4),
-	   .b0(P1A7),
-	   .b1(P1A8),
-	   .b2(P1A9),
-	   .b3(P1A10),
-	   .g0(P1B1),
-	   .g1(P1B2),
-	   .g2(P1B3),
-	   .g3(P1B4),
-	   .hs(P1B7),
-	   .vs(P1B8));
+  // icepll -o 25.175 -m -p
+  /*
+   * PLL configuration
+   *
+   * This Verilog module was generated automatically
+   * using the icepll tool from the IceStorm project.
+   * Use at your own risk.
+   *
+   * Given input frequency:        12.000 MHz
+   * Requested output frequency:   25.175 MHz
+   * Achieved output frequency:    25.125 MHz
+   */
+  SB_PLL40_PAD #(
+		         .FEEDBACK_PATH("SIMPLE"),
+		         .DIVR(4'b0000),		// DIVR =  0
+		         .DIVF(7'b1000010),	// DIVF = 66
+		         .DIVQ(3'b101),		// DIVQ =  5
+		         .FILTER_RANGE(3'b001)	// FILTER_RANGE = 1
+		         ) pll (
+			            .LOCK(locked),
+			            .RESETB(1'b1),
+			            .BYPASS(1'b0),
+			            .PACKAGEPIN(CLK),
+			            .PLLOUTCORE(clk_25_125)
+			            );
+  // POR delay
+  always @ (posedge clk_25_125) begin
+    if (rst_counter != 0) begin
+	  rst <= 1'b1;
+	  if (locked) begin
+	    rst_counter <= rst_counter - 1;
+	  end
+    end else begin
+	  rst <= 1'b0;
+    end
+  end
+
+  vga vga(.clk(clk_25_125), 
+	      .rst(rst),
+	      .r0(P1A1),
+	      .r1(P1A2),
+	      .r2(P1A3),
+	      .r3(P1A4),
+	      .b0(P1A7),
+	      .b1(P1A8),
+	      .b2(P1A9),
+	      .b3(P1A10),
+	      .g0(P1B1),
+	      .g1(P1B2),
+	      .g2(P1B3),
+	      .g3(P1B4),
+	      .hs(P1B7),
+	      .vs(P1B8));
 endmodule
--- src/vga.v
+++ src/vga.v
@@ -1,72 +1,119 @@
 module vga(
-	   input  clk,
-	   input  rst,
-	   output r0,
-	   output r1,
-	   output r2,
-	   output r3,
-	   output g0,
-	   output g1,
-	   output g2,
-	   output g3,
-	   output b0,
-	   output b1,
-	   output b2,
-	   output b3,
-	   output hs,
-	   output vs
-	   );
+	       input  clk,
+	       input  rst,
+	       output r0,
+	       output r1,
+	       output r2,
+	       output r3,
+	       output g0,
+	       output g1,
+	       output g2,
+	       output g3,
+	       output b0,
+	       output b1,
+	       output b2,
+	       output b3,
+	       output hs,
+	       output vs
+	       );
 
-   localparam	  max_h = 640;
-   localparam	  max_v = 480;
+  localparam	  h_visible    = 640;
+  localparam	  h_frontporch = 640 + 24;
+  localparam	  h_sync       = 640 + 24 + 95;
+  localparam	  h_backporch  = 640 + 24 + 95 + 48;
 
-   // 2^10  = 1024
-   reg [9:0]	  count_h;
-   // 2^9 = 512
-   reg [8:0]	  count_v;
-   reg		  red;
-   reg		  grn;
-   reg		  blu;
-   reg		  hs_out;
-   reg		  vs_out;
+  // while h_sync is being sent, vertical counts h_sync pulses, for
+  // porch and sync, it counts clocks
+  localparam	  v_visible    = 480;
+  localparam	  v_frontporch = 480 + 13847;
+  localparam	  v_sync       = 480 + 13847 + 1612;
+  localparam	  v_backporch  = 480 + 13847 + 1612 + 504;
 
-   assign r0 = red;
-   assign r1 = red;
-   assign r2 = red;
-   assign r3 = red;
-   assign b0 = blu;
-   assign b1 = blu;
-   assign b2 = blu;
-   assign b3 = blu;
-   assign g0 = grn;
-   assign g1 = grn;
-   assign g2 = grn;
-   assign g3 = grn;
-   assign hs = hs_out;
-   assign vs = vs_out;
+  // 2^10  = 1024
+  reg [9:0]       count_h;
+  // 2^15 = 32768
+  reg [14:0]      count_v;
+  reg             red;
+  reg             grn;
+  reg             blu;
+  reg             hs_out;
+  reg             vs_out;
 
-   always @ (posedge clk) begin
-      if (rst) begin
-         count_h <= 0;
-	 count_v <= 0;
-	 red <= 1'b0;
-	 grn <= 1'b0;
-	 blu <= 1'b0;
-      end else if (count_h != max_h) begin
-	 hs_out <= 1'b0;
-	 vs_out <= 1'b0;
-         count_h <= count_h + 1;
-      end else begin
-         count_h <= 0;
-	 hs_out <= 1'b1;
-         if (count_v != max_v) begin
-            count_v <= count_v + 1;
-         end else begin
-            count_v <= 0;
+  assign r0 = red;
+  assign r1 = red;
+  assign r2 = red;
+  assign r3 = red;
+  assign b0 = blu;
+  assign b1 = blu;
+  assign b2 = blu;
+  assign b3 = blu;
+  assign g0 = grn;
+  assign g1 = grn;
+  assign g2 = grn;
+  assign g3 = grn;
+  assign hs = hs_out;
+  assign vs = vs_out;
+
+  always @ (posedge clk) begin
+    hs_out <= 1'b0;
+    vs_out <= 1'b0;
+    if (rst) begin
+      count_h <= 10'b1;
+	  count_v <= 9'b1;
+    end else if (count_h < h_visible) begin
+      count_h <= count_h + 1;
+	  // horizontal visible
+	  red <= 1'b1;
+	  grn <= 1'b1;
+	  blu <= 1'b1;
+    end else if (count_h < h_frontporch) begin
+      count_h <= count_h + 1;
+	  // horizontal front porch
+	  red <= 1'b0;
+	  grn <= 1'b0;
+	  blu <= 1'b0;
+    end else if (count_h < h_sync) begin
+      count_h <= count_h + 1;
+	  // horizontal sync
+	  hs_out <= 1'b1;
+	  red <= 1'b0;
+	  grn <= 1'b0;
+	  blu <= 1'b0;
+    end else if (count_h < h_backporch-1) begin
+      count_h <= count_h + 1;
+	  // horizontal back porch
+	  red <= 1'b0;
+	  grn <= 1'b0;
+	  blu <= 1'b0;
+    end else begin
+      if (count_v < v_visible) begin
+        count_v <= count_v + 1;
+	    // vertical visible
+        count_h <= 0;
+      end else if (count_v < v_frontporch) begin
+        count_v <= count_v + 1;
+	    // vertical front porch
+	    red <= 1'b0;
+	    grn <= 1'b0;
+	    blu <= 1'b0;
+      end else if (count_v < v_sync) begin
+        count_v <= count_v + 1;
+	    // vertical sync
 	    vs_out <= 1'b1;
-         end
+	    red <= 1'b0;
+	    grn <= 1'b0;
+	    blu <= 1'b0;
+	  end else if (count_v < v_backporch-1) begin
+        count_v <= count_v + 1;
+	    // vertical back porch
+	    red <= 1'b0;
+	    grn <= 1'b0;
+	    blu <= 1'b0;
+	  end else begin
+        count_v <= 0;
       end
-   end
+    end
+  end
 
 
 endmodule
