fcb821993ac748423d5efbbc53f1d469dcd5f5ae
Tom Keddie <github@bronwenandtom.com>
2023-10-28 14:45:13 -0700

    Add bsod data

---
+++ src/data/parse.py
@@ -0,0 +1,11 @@
+import png
+
+r = png.Reader(filename='640px-BSOD_Windows_8.svg.1bit.png')
+(width, height, rows, info) = r.read()
+for row in rows:
+    if (sum(row) != 0):
+        for pixel in row:
+            print("{}".format(pixel), end='')
+        print()
+
+
