f08ecb96c1f9f34581eb9422deaeaa1d5e82303d
Tom Keddie <github@bronwenandtom.com>
2023-10-29 14:57:56 -0700

    Reduce text size to 54 lines

--- src/data/parse.py
+++ src/data/parse.py
@@ -1,6 +1,6 @@
 import png
 
-r = png.Reader(filename='640px-BSOD_Windows_8.svg.1bit.png')
+r = png.Reader(filename='640px-BSOD_Windows_8.svg.1bit.2.png')
 (width, height, rows, info) = r.read()
 for row in rows:
     if (sum(row) != 0):
