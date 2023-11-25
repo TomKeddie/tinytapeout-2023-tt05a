ca25e1f653f16edc8c29e0d76186052de900bea1
Tom Keddie <github@bronwenandtom.com>
2023-11-25 08:13:54 -0800

    Add vivado files

---
+++ src/arty_vivado/arty_vivado.gen/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
+// Date        : Wed Nov 15 17:49:41 2023
+// Host        : x570 running 64-bit Ubuntu 22.04.3 LTS
+// Command     : write_verilog -force -mode synth_stub -rename_top clk_wiz_0 -prefix
+//               clk_wiz_0_ clk_wiz_0_stub.v
+// Design      : clk_wiz_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35ticsg324-1L
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module clk_wiz_0(clk_out1, reset, locked, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,reset,locked,clk_in1" */;
+  output clk_out1;
+  input reset;
+  output locked;
+  input clk_in1;
+endmodule
